15c2380247b3dae2d0e530238bb6505fdb16cf68
Paulo Sergio <pauloserg861@gmail.com>
2026-07-19 18:19:48 -0300

    feat: feat: ensure BcryptAdapter call compare hash with correct values and return true on success

--- src/shared/infrastructure/adapters/bcrypt/bcrypt.adapter.ts
+++ src/shared/infrastructure/adapters/bcrypt/bcrypt.adapter.ts
@@ -1,10 +1,15 @@
 import bcrypt from 'bcrypt';
+import { CompareHashPort } from '../../../application/ports/compare-hash.port';
 import { EncrypterPort } from '../../../application/ports/encrypter.port';
 
-export class BcryptAdapter implements EncrypterPort {
+export class BcryptAdapter implements EncrypterPort, CompareHashPort {
   async encrypt(value: string): Promise<string> {
     const salt = 10;
     const hashedValue = await bcrypt.hash(value, salt);
     return hashedValue;
+  }
+
+  async compare(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
   }
 }
