f1d658ad655837bc4152faa3227c5c1a932ea299
Paulo Sergio <pauloserg861@gmail.com>
2026-07-19 23:20:33 -0300

    feat: ensure correct behavior for GetEmployeesController

---
+++ src/modules/employees/presentation/controllers/get-employees.controller.ts
@@ -0,0 +1,39 @@
+import {
+  GetEmployeesDto,
+  GetEmployeesResultDto,
+} from '@modules/employees/application/dtos/get-employees.dto';
+import { GetEmployeesPort } from '@modules/employees/application/ports/inbound/get-employees.port';
+import {
+  HttpErrorBody,
+  HttpSuccessBody,
+  ok,
+  serverError,
+} from '@shared/presentation/helpers/http-helper';
+import { BaseController } from '@shared/presentation/protocols/base-controller';
+import { HttpResponse } from '@shared/presentation/protocols/http-response';
+
+export class GetEmployeesController extends BaseController<
+  GetEmployeesDto,
+  HttpErrorBody | HttpSuccessBody<GetEmployeesResultDto>
+> {
+  constructor(private readonly getEmployees: GetEmployeesPort) {
+    super();
+  }
+
+  async handle(
+    request: GetEmployeesDto,
+  ): Promise<
+    HttpResponse<HttpErrorBody | HttpSuccessBody<GetEmployeesResultDto>>
+  > {
+    try {
+      const employees = await this.getEmployees.execute({
+        isActive: request.isActive,
+        role: request.role,
+      });
+
+      return ok({ employees });
+    } catch (error) {
+      return serverError(error as Error);
+    }
+  }
+}
